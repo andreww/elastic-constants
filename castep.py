andreww/elastic-constants

commit c3e48921d6b50f6dcd77ed7c74d88bf6efe73dd1

Move castep regex out of analysis script
This should make it easer to use with other
code.

castep.py
@@ -9,6 +9,7 @@ All rights reserved.
 """
 
 import re
+import scipy as S
 
 version = 0.1
 
@@ -106,4 +107,25 @@ def produce_dotcell(seedname, filename, defcell, atoms):
 	inputfile.close
 	outputfile.close
 	return()
-			
+
+	# regular expression which matches the whole stress tensor block from a .castep file
+	stressRE = re.compile("\s\*+\s(?:Symmetrised\s)?Stress\sTensor\s\*+\n.+\n.+?\((\w+)\).+\n.+\n.+\n.+\n\s\*\s+x\s+([\+\-]?\d+.\d+)\s+([\+\-]?\d+.\d+)\s+([\+\-]?\d+.\d+)\s+\*\n\s\*\s+y\s+[\+\-]?\d+.\d+\s+([\+\-]?\d+.\d+)\s+([\+\-]?\d+.\d+)\s+\*\n\s\*\s+z\s+[\+\-]?\d+.\d+\s+[\+\-]?\d+.\d+\s+([\+\-]?\d+.\d+)\s+\*\n")
+
+def get_stress_dotcastep(filename):
+	"""Extract the stress tensor from a .castep file
+	
+	   Returns a tuple of (<units>, <stress>) where <units>
+	   is a string representing the stress units and 
+	   <stress> is a numpy vector of the elements of the 
+	   stress tensor in the order s(1,1), s(2,2), s(3,3)
+	   s(3,2), s(3,1), s(2,1).
+	"""
+	dotCastep = open(filename,"r")
+	stressData = stressRE.findall(dotCastep.read())[0]
+	dotCastep.close()
+	units = stressData[0]
+	stress = S.array([float(stressData[1]),float(stressData[4]),
+                          float(stressData[6]),float(stressData[5]),
+                          float(stressData[3]),float(stressData[2])])
+	return(units, stress)
+

elastics.py
@@ -14,6 +14,7 @@ import re
 import optparse
 import scipy as S
 import CijUtil
+import castep
 
 version = 1
 	
@@ -149,9 +150,6 @@ def main(input_options, libmode=False):
 	latticeTypes = {0:"Unknown", 1:"Triclinic", 2:"Monoclinic", 3:"Orthorhombic", \
 		4:"Tetragonal", 5:"Cubic", 6:"Trigonal-low", 7:"Trigonal-high/Hexagonal"}
 
-	# regular expression which matches the whole stress tensor block from a .castep file
-	stressRE = re.compile("\s\*+\s(?:Symmetrised\s)?Stress\sTensor\s\*+\n.+\n.+?\((\w+)\).+\n.+\n.+\n.+\n\s\*\s+x\s+([\+\-]?\d+.\d+)\s+([\+\-]?\d+.\d+)\s+([\+\-]?\d+.\d+)\s+\*\n\s\*\s+y\s+[\+\-]?\d+.\d+\s+([\+\-]?\d+.\d+)\s+([\+\-]?\d+.\d+)\s+\*\n\s\*\s+z\s+[\+\-]?\d+.\d+\s+[\+\-]?\d+.\d+\s+([\+\-]?\d+.\d+)\s+\*\n")
-
 	# Get strain tensors
 	seedname = arguments[0]
 
@@ -220,19 +218,14 @@ def main(input_options, libmode=False):
 				strain = S.row_stack((strain,S.array([float(line1[0]),float(line2[1]),float(line3[2]),2*float(line2[2]),2*float(line1[2]),2*float(line1[1])])))
 		
 			# now get corresponding stress data from .castep
-			dotCastep = open(seedname+"_cij__"+str(patt+1)+"__"+str(a+1)+".castep","r")	
-			stressData = stressRE.findall(dotCastep.read())[0]
-			dotCastep.close()
-		
-			units = stressData[0]
+                        (units, thisStress) = castep.get_stress_dotcastep(seedname+
+				"_cij__"+str(patt+1)+"__"+str(a+1)+".castep")
 	
 			# again, top right triangle
 			if a == 0:
-				stress = S.array([float(stressData[1]),float(stressData[4]),float(stressData[6]),float(stressData[5]),float(stressData[3]),float(stressData[2])])
+				stress = thisStress
 			else:
-				stress = S.row_stack((stress,S.array([float(stressData[1]),float(stressData[4]),float(stressData[6]),float(stressData[5]),float(stressData[3]),float(stressData[2])])))		
-			
-
+				stress = S.row_stack(thisStress)
 	
 
 		"""

commit 9dfa382073b44324eda05ba041281d86e0ab6298

Fix bugs introduced by c3e48921d6
Avoid loosing stresses and make sure
the Castep regular expressions are
defined

castep.py
@@ -108,8 +108,8 @@ def produce_dotcell(seedname, filename, defcell, atoms):
 	outputfile.close
 	return()
 
-	# regular expression which matches the whole stress tensor block from a .castep file
-	stressRE = re.compile("\s\*+\s(?:Symmetrised\s)?Stress\sTensor\s\*+\n.+\n.+?\((\w+)\).+\n.+\n.+\n.+\n\s\*\s+x\s+([\+\-]?\d+.\d+)\s+([\+\-]?\d+.\d+)\s+([\+\-]?\d+.\d+)\s+\*\n\s\*\s+y\s+[\+\-]?\d+.\d+\s+([\+\-]?\d+.\d+)\s+([\+\-]?\d+.\d+)\s+\*\n\s\*\s+z\s+[\+\-]?\d+.\d+\s+[\+\-]?\d+.\d+\s+([\+\-]?\d+.\d+)\s+\*\n")
+# regular expression which matches the whole stress tensor block from a .castep file
+stressRE = re.compile("\s\*+\s(?:Symmetrised\s)?Stress\sTensor\s\*+\n.+\n.+?\((\w+)\).+\n.+\n.+\n.+\n\s\*\s+x\s+([\+\-]?\d+.\d+)\s+([\+\-]?\d+.\d+)\s+([\+\-]?\d+.\d+)\s+\*\n\s\*\s+y\s+[\+\-]?\d+.\d+\s+([\+\-]?\d+.\d+)\s+([\+\-]?\d+.\d+)\s+\*\n\s\*\s+z\s+[\+\-]?\d+.\d+\s+[\+\-]?\d+.\d+\s+([\+\-]?\d+.\d+)\s+\*\n")
 
 def get_stress_dotcastep(filename):
 	"""Extract the stress tensor from a .castep file

elastics.py
@@ -225,7 +225,7 @@ def main(input_options, libmode=False):
 			if a == 0:
 				stress = thisStress
 			else:
-				stress = S.row_stack(thisStress)
+				stress = S.row_stack((stress,thisStress))
 	
 
 		"""