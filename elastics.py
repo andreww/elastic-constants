Fix error calculation
New (> 0.10.0) versions of scipy were incorrectly changing the calculation
of the error on the elastic constants due to a change in scipy.stats.linres
before version 0.7.0. This corrects the test and directly uses the standard
error from the fit.

Thanks to Keith Refson for reporting this and for providing the fix.

elastics.py
@@ -242,18 +242,16 @@ def main(input_options, libmode=False):
 			# do the fit
 			(cijFitted,intercept,r,tt,stderr) = stats.linregress(strain[:,index2-1],stress[:,index1-1])
 
-			if (S.__version__ < '0.7.0'):
+			(vmajor,vminor,vmicro) = re.split('\.',S.__version__)
+
+			if ( vmajor > 0 or vminor >= 7):
+				error = stderr
+			else:
 				# correct for scipy weirdness - see http://www.scipy.org/scipy/scipy/ticket/8
 				# This was fixed before 0.7.0 release. Maybe in some versions of 0.6.x too - 
 				# will report huge errors if the check is wrong
 				stderr = S.sqrt((numsteps * stderr**2)/(numsteps-2))
 				error  = stderr/sqrt(sum(square(strain[:,index2-1])))
-			else:
-				# Work out the error ourselves as I cannot get it from
-				# stderr and this has been checked with gnuplot's fitter
-				fit_str = ((strain[:,index2-1] * cijFitted) + intercept)
-				error = sqrt((sum(square(stress[:,index1-1] - fit_str)) / \
-				             (numsteps-2))/(sum(square(strain[:,index2-1]))))
 			
 			# print info about the fit
 			print '\n'